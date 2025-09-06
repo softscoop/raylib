82097211649b2f987e5bccb8f0055fdf7b0b6be6
softscoop <164802848+softscoop@users.noreply.github.com>
2025-09-06 10:10:04 +0100

    clean up and basic win condition/detection

--- multi/src/tictactoe.c
+++ multi/src/tictactoe.c
@@ -6,6 +6,9 @@ extern bool switchGame;
 extern int selection;
 extern bool gameFirstFrame;
 
+bool CheckWinX();
+bool CheckWinO();
+
 static const int screenWidth = 480;
 static const int screenHeight = 360;
 static const Vector2 centerPoint = {screenWidth / 2, screenHeight / 2};
@@ -13,6 +16,7 @@ static const int centerX = 240 - 40;
 static const int centerY = 180 - 40;
 static bool player1 = true;
 static Font myFont;
+static bool gameOver = false;
 
 static Texture2D background;
 static char choices[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
@@ -37,6 +41,7 @@ void TicInput(void){
             for (int e = 0; e < 3; e++){
                 choices[i][e] = ' ';
                 player1 = true;
+                gameOver = false;
             }
         }
     }
@@ -49,15 +54,11 @@ void TicUpdate(void){
         for (int i = 0; i < 3; i++){
             for (int e = 0; e < 3; e++){
                 if (CheckCollisionPointRec(GetMousePosition(), gridBoxes[i][e])){
-                    if (choices[i][e] == ' '){
-                        if (player1 && (choices[i][e] != 'X' || choices[i][e] != 'O')){
-                            choices[i][e] = 'X';
-                            player1 = !player1;
-                        }
-                        else if (!player1 && (choices[i][e] != 'X' || choices[i][e] != 'O')){
-                            choices[i][e] = 'O';
-                            player1 = !player1;
-                        }
+                    if (choices[i][e] == ' ' && !gameOver){
+                        if (player1) choices[i][e] = 'X';
+                        else choices[i][e] = 'O';
+                        player1 = !player1;
+                        if (CheckWinX() || CheckWinO()) gameOver = true;
                     }
                 }
             }
@@ -84,4 +85,32 @@ void TicDraw(void){
         switchGame = true;
         selection = 0;
     } 
+}
+
+bool CheckWinX(){
+    if (choices[0][0] == 'X' && choices[0][1] == 'X' && choices[0][2] == 'X') return true;
+    if (choices[1][0] == 'X' && choices[1][1] == 'X' && choices[1][2] == 'X') return true;
+    if (choices[2][0] == 'X' && choices[2][1] == 'X' && choices[2][2] == 'X') return true;
+
+    if (choices[0][0] == 'X' && choices[1][0] == 'X' && choices[2][0] == 'X') return true;
+    if (choices[0][1] == 'X' && choices[1][1] == 'X' && choices[2][1] == 'X') return true;
+    if (choices[0][2] == 'X' && choices[1][2] == 'X' && choices[2][2] == 'X') return true;
+
+    if (choices[0][0] == 'X' && choices[1][1] == 'X' && choices[2][2] == 'X') return true;
+    if (choices[0][2] == 'X' && choices[1][1] == 'X' && choices[2][0] == 'X') return true;
+    return false;
+}
+
+bool CheckWinO(){
+    if (choices[0][0] == 'O' && choices[0][1] == 'O' && choices[0][2] == 'O') return true;
+    if (choices[1][0] == 'O' && choices[1][1] == 'O' && choices[1][2] == 'O') return true;
+    if (choices[2][0] == 'O' && choices[2][1] == 'O' && choices[2][2] == 'O') return true;
+
+    if (choices[0][0] == 'O' && choices[1][0] == 'O' && choices[2][0] == 'O') return true;
+    if (choices[0][1] == 'O' && choices[1][1] == 'O' && choices[2][1] == 'O') return true;
+    if (choices[0][2] == 'O' && choices[1][2] == 'O' && choices[2][2] == 'O') return true;
+
+    if (choices[0][0] == 'O' && choices[1][1] == 'O' && choices[2][2] == 'O') return true;
+    if (choices[0][2] == 'O' && choices[1][1] == 'O' && choices[2][0] == 'O') return true;
+    return false;
 }
